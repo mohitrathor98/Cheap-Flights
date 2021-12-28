c8b43b561e4d2fe3e91adce7c2b911cd30a57f34
mohit <mohitrathor8@gmail.com>
2021-12-28 15:57:41 +0530

    reading spreadsheet

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,13 @@
+import requests
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    
+***REMOVED***
+        self.sheety_url = "https://api.sheety.co/407a967edb3e231ee6c7f637d95bbfb6/flightDeals/prices"
+        self.sheety_header = { "Authorization": "Bearer igotyoumoonlightyouremystarlight21323cmmondancewithme" } 
+        self.tequila_url = "https://tequila-api.kiwi.com"
+        self.tequila_key = "e4X5g2dGr4YXwfUgIbAs92hVwAPhS7e0"
+        
+    def read_from_sheet(self):
+        response = requests.get(url=self.sheety_url, headers=self.sheety_header)
+        response.raise_for_status
+        return response.json()
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+spreadsheet = DataManager()
+print(spreadsheet.read_from_sheet())
