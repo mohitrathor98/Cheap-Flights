41711a333e7c6195893fd9274f135ae1a326bbd2
mohit <mohitrathor8@gmail.com>
2021-12-30 13:36:47 +0530

    some todos

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_data import FlightData
+
+# TODO:
+# export all the tokens and ids in form of string dictionary
+# and use json.loads to convert it back
+
+# ask user departure city name
 
 spreadsheet = DataManager()
 spreadsheet.insert_iata()
